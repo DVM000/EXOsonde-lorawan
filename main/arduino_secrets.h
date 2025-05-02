72cbfd2a37cd845bd7c29730723d9305031dfdbf
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-05-01 22:00:42 -0500

    fix: Update comment for SECRET_APP_EUI in arduino_secrets.h to clarify usage

--- main/arduino_secrets.h
+++ main/arduino_secrets.h
@@ -1,3 +1,3 @@
 // Replace with keys obtained from Chirpstack
-#define SECRET_APP_EUI "0000000000" // if using OTAA, leave empty
+#define SECRET_APP_EUI "0000000000" // if using OTAA, leave it as 0s
 #define SECRET_APP_KEY "yyyyyyyyyyyyyyyyyyyyyyy"
